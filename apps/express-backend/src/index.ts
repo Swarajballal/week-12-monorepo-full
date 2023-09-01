102dbf12d226eaa5728c64c1442734a39decf1a0
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-09-01 22:12:02 +0530

    Week 12

--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -3,6 +3,8 @@ const app = express()
 const port = 3002
 import { UserInput } from "common"
 
+app.use(express.json());
+
 app.get('/', (req: any, res: any) => {
   let parsedUser = UserInput.safeParse(req.body);
   if (!parsedUser.success) {
